97c10bc3e9eaa9f303c83a1621cdc9712711b614
Katherine Domingo <katherine.domingo@torchbox.com>
2023-06-27 15:30:50 +0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import re
 import os
 import sys
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 name = 'wagtailmodelchooser'
@@ -23,46 +23,7 @@ def get_version(package):
     return re.search("^__version__ = ['\"]([^'\"]+)['\"]",
                      init_py, re.MULTILINE).group(1)
 
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
-
-
 version = get_version(package)
-
-
-if sys.argv[-1] == 'publish':
-    if os.system("pip freeze | grep wheel"):
-        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
-        sys.exit()
-    os.system("python setup.py sdist upload")
-    os.system("python setup.py bdist_wheel upload")
-    print("You probably want to also tag the version now:")
-    print("  git tag -a {0} -m 'version {0}'".format(version))
-    print("  git push --tags")
-    sys.exit()
-
 
 install_requires = [
     'Django>=3.2',
@@ -95,28 +56,11 @@ setup(
     description=description,
     author=author,
     author_email=author_email,
-    packages=get_packages(package),
-    package_data=get_package_data(package),
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=install_requires,
     extras_require={
         'testing': testing_extras,
         'docs': documentation_extras
     },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Natural Language :: English',
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Framework :: Django",
-        "Framework :: Django :: 3",
-        "Framework :: Django :: 4",
-        "Framework :: Wagtail",
-        "Framework :: Wagtail :: 4",
-        "Framework :: Wagtail :: 5",
-        "License :: OSI Approved :: BSD License",
-    ]
 )
